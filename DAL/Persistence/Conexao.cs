0b6d2ddea57db1172cb364c5b0d2a4e80a8c5745
Elis Fernanda Agape Santos Vieira <elisvieira@add.addtech.com.br>
2017-01-03 15:59:57 -0200

    Projeto inicial

---
+++ DAL/Persistence/Conexao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;//ADO.NET -> SQL SERVER
+
+namespace DAL.Persistence
+{
+   public class Conexao
+    {
+        //Atributos:
+        protected SqlConnection Con; // Estabelecer a conexão com sql server
+        protected SqlCommand Cmd;// escrever e executar o comando de sql
+        protected SqlDataReader Dr; // ler registro obtidos de queres(trabalhando com as consultas)
+
+        //Método - Abrir conexão
+        protected void AbrirConexao()
+        {
+            try
+            {
+                //buscar a connection string
+                Con = new SqlConnection("Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\elisvieira\Documents\ADD-WS0430.mdf;Integrated Security=True;Connect Timeout=30"); 
+                Con.Open();//abrindo a conexao
+            }
+            catch (Exception EX)
+            {
+
+                throw new Exception(EX.Message);
+            }
+        }
+
+        protected void FecharConexao()
+        {
+            try
+            {
+                Con.Close();
+            }
+            catch (Exception EX)
+            {
+
+                throw new Exception(EX.Message);
+            }
+        }
+
+    }
+}
---
+++ DAL/Persistence/PessaoDAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DAL.Model;
+
+namespace DAL.Persistence
+{
+    public class PessaoDAL : Conexao
+    {
+        public void Gravar(Pessoa p)
+        {
+            try
+            {
+                //Abrir a conexão
+                AbrirConexao();
+
+                Cmd = new SqlCommand("insert into Pessoa (Nome,Endereco,Email) values(@v1,@v2,@v3)", Con );
+
+                Cmd.Parameters.AddWithValue("@v1", p.Nome);
+                Cmd.Parameters.AddWithValue("@v2", p.Endereco);
+                Cmd.Parameters.AddWithValue("@v3", p.Email);
+
+                Cmd.ExecuteNonQuery();//Executando o código
+                
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Erro ao gravar o cliente: " + e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public void Atualizar(Pessoa p)
+        {
+            try
+            {
+                AbrirConexao();
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Erro ao atualizar o cliente: " + e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+    }
+}
+//regras de negocios da nossa aplicacao - consultas! - select, update,insert,delete 
