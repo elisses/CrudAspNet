a042f5793ca30e8d85946a803b5b4f2e7c1d257f
Elis Fernanda Agape Santos Vieira <elisvieira@add.addtech.com.br>
2017-01-04 13:02:25 -0200

    Adicionando o crud

--- DAL/Persistence/PessaoDAL.cs
+++ DAL/Persistence/PessaoDAL.cs
@@ -42,6 +42,16 @@ namespace DAL.Persistence
             try
             {
                 AbrirConexao();
+
+                Cmd = new SqlCommand("update Pessoa set Nome=@v1, Endereco=@v2, Email=@v3 where Codigo=@v4", Con);
+
+                Cmd.Parameters.AddWithValue("@v1", p.Nome);
+                Cmd.Parameters.AddWithValue("@v2", p.Endereco);
+                Cmd.Parameters.AddWithValue("@v3", p.Email);
+                Cmd.Parameters.AddWithValue("@v4", p.Codigo);
+
+                Cmd.ExecuteNonQuery();
+
             }
             catch (Exception e)
             {
@@ -53,6 +63,105 @@ namespace DAL.Persistence
                 FecharConexao();
             }
         }
+
+        public void Excluir(int Codigo)
+        {
+            try
+            {
+                AbrirConexao();
+
+                Cmd = new SqlCommand("delete from Pessoa where Codigo=@v1", Con);
+
+                Cmd.Parameters.AddWithValue("@v1", Codigo);
+
+                Cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Erro ao excluir o cliente: " + e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        //metodo pra obter 1 pessoa pelo Codigo(chave primária)
+        public Pessoa PesquisarPorCodigo(int Codigo)
+        {
+            try
+            {
+                AbrirConexao();
+
+                Cmd = new SqlCommand("select * from Pessoa where Codigo=@v1", Con);
+
+                Cmd.Parameters.AddWithValue("@v1", Codigo);
+
+                Pessoa p = null; // criando um espaço de memória. ponteiro
+
+                if (Dr.Read())//Se SqlDataReader Dr for  ler os registros
+                {
+                    p = new Pessoa(); // inicializar o objeto Pessoa
+
+                    p.Codigo = Convert.ToInt32(Dr["Codigo"]);
+                    p.Nome = Convert.ToString(Dr["Nome"]);
+                    p.Endereco = Convert.ToString(Dr["Endereco"]);
+                    p.Email = Convert.ToString(Dr["Email"]);
+                }
+                return p;
+            }
+            catch (Exception e )
+            {
+
+                throw new Exception("Erro ao pesquisar: " + e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+
+        public List<Pessoa> Listar()
+        {
+            try
+            {
+                AbrirConexao();
+
+                Cmd = new SqlCommand("Select 8 from Pessoa", Con);
+
+                Dr = Cmd.ExecuteReader();
+
+                List<Pessoa> lista = new List<Pessoa>(); // lista vazia
+
+                while (Dr.Read())
+                {
+                    Pessoa p = new Pessoa();
+
+                    p.Codigo = Convert.ToInt32(Dr["Codigo"]);
+                    p.Nome = Convert.ToString(Dr["Nome"]);
+                    p.Endereco = Convert.ToString(Dr["Endereco"]);
+                    p.Email = Convert.ToString(Dr["Email"]);
+
+                    lista.Add(p);
+                }
+                return lista;
+
+                
+
+            }
+            catch (Exception e)
+            {
+
+                throw new Exception("Erro ao listar os clientes: " + e.Message);
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+        
     }
 }
  //regras de negocios da nossa aplicacao - consultas! - select, update,insert,delete 
